d075b8112521ae96c515067f09f95057b7ced3ce
Markus Bauer <ma88bauer@gmail.com>
2021-05-28 15:38:18 -0700

    Wrote third test: checks that function prints buzz for multiples of 5 and not 3

--- fizzbuzztest.py
+++ fizzbuzztest.py
@@ -5,13 +5,18 @@ class testfb(unittest.TestCase):
 
     def test_printnumbers(self):
         for x in range(1, 101):
-            if x % 3 != 0 or x % 5 != 0:
+            if x % 3 != 0 and x % 5 != 0:
                 self.assertEqual(fizzbuzz.fb(x), x)
 
     def test_printfizz(self):
         for x in range(1, 101):
             if x % 3 == 0 and x % 5 != 0:
                 self.assertEqual(fizzbuzz.fb(x), "Fizz")
+
+    def test_printfizz(self):
+        for x in range(1, 101):
+            if x % 3 != 0 and x % 5 == 0:
+                self.assertEqual(fizzbuzz.fb(x), "Buzz")
 
 if __name__ == "__main__":
     unittest.main()
