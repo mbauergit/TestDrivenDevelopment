5994923e82aff97f3c8f5de6796e3e73446ba406
Markus Bauer <ma88bauer@gmail.com>
2021-05-28 15:54:45 -0700

    Implemented div by 400 method

--- leapyear.py
+++ leapyear.py
@@ -1,6 +1,8 @@
 def ly(x):
     if x % 4 == 0:
         if x % 100 == 0:
+            if x % 400 == 0:
+                return "Is a leap year"
             return "Is not a leap year"
         return "Is a leap year"
     else:
