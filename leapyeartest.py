dd0f8dc2cc1d5e6c56fec76e39c8b0442080c512
Markus Bauer <ma88bauer@gmail.com>
2021-05-28 15:53:20 -0700

    Wrote third test: checks that years div by 400 are leapyears

--- leapyeartest.py
+++ leapyeartest.py
@@ -11,6 +11,10 @@ class testly(unittest.TestCase):
     def test_divby100(self):
         self.assertEqual(leapyear.ly(2100), "Is not a leap year")
         self.assertEqual(leapyear.ly(1900), "Is not a leap year")
-        
+
+    def test_divby400(self):
+        self.assertEqual(leapyear.ly(2000), "Is a leap year")
+        self.assertEqual(leapyear.ly(2400), "Is a leap year")
+
 if __name__ == "__main__":
     unittest.main()
